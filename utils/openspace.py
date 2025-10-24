85f3d285656913d468834dd23a710c166320e84d
Faranges <farangesm2@gmail.com>
2025-10-24 15:51:47 +0200

    Project Openspace Classifier

--- utils/openspace.py
+++ utils/openspace.py
@@ -1,1 +1,81 @@
+import random                     # use this to randomly shuffle the list of names
 
+from utils.table import Table     # This import allows us to use Table and Seat objects inside our OpenSpace class   
+
+class OpenSpace:
+    """ 
+    Represents an open workspace containing multiple tables.
+
+    Attributes: 
+        number_of_tables(int): Number of tables in the space
+        tables(list): A list of Table objects
+    """
+    def __init__(self, number_of_tables: int = 6) -> None:
+        # Save the number of tables as an attribute 
+        self.number_of_tables = number_of_tables
+
+        # Create an empty list that will hold all the Table objects
+        self.tables = []
+
+        # Loop as many times as there are tables
+        for i in range(number_of_tables):
+            # Create one new Table object (each table will have its own seats)
+            table = Table()
+
+            # Add the newly created Table to the list of tables
+            self.tables.append(table)
+    
+    def organize(self,names: list[str]) -> None:
+        """Randomly assign people to available seats across all tables."""
+        
+        random.shuffle(names)                                   # Shuffle the list of names so the order is random
+        name_index = 0                                          # Keep track of which person we're assigning
+        for table in self.tables:                               # Loop through all the tables in OpenSpace
+            for seat in table.seats:                            # Loop through each seat in the current table
+                if name_index < len(names) and seat.free:       # If there are still people left to assign AND seat is free
+                    seat.set_occupant(names[name_index])        # Assign this person
+                    name_index += 1                             # Move to the next person
+        
+        if name_index < len(names):
+            print("Warning:some people could net be seated.")   # If there were more people than seats, print a warning
+        else:
+            print("Everyone has been seated successfully.")
+
+    def display(self) -> None:
+        """SHow  all tables and their occupants in a readable way."""
+        print("\n Open Space Seating Plan")
+        
+        for i, table in enumerate(self.tables, start=1):        # Go through all tables one by one
+            print(f"Table {i}:")                             # Display the table number
+
+            for j, seat in enumerate(table.seats, start=1):     # Go through all seats in this table
+                if seat.free:
+                    print(f"  Seat {j}: [Empty]")
+                else:
+                    print(f"  Seat {j}: {seat.occupant}")
+
+        print("End of Seating Plan\n")
+
+    def store(self,filename: str) -> None:
+        """Save the current seating arrangement into a text file."""
+        with open (filename, "w") as file:                      # Open a file in write mode
+            file.write("Open Space Seating Plan\n")             # Write a title at the top
+            
+            for i, table in enumerate(self.tables, start=1):    # Go through each table and each seat
+                file.write(f"Table {i}:")
+                for j, seat in enumerate(table.seats, start=1):
+                    if seat.free:
+                        file.write(f"  Seat {j}: [Empty]")
+                    else:
+                        file.write(f"  Seat {j}: {seat.occupant}")
+
+        print(f"Seating plan saved successfully in '{filename}'.")
+
+    def __str__(self) -> str:
+        """Short summary of the OpenSpace"""
+        return f"Openspace with {len(self.tables)} tables."
+            
+
+
+
+        
--- utils/table.py
+++ utils/table.py
@@ -1,1 +1,105 @@
+"""
+table.py
+Contains the Seat and Table classes.
+ """
+class Seat:
+    """
+    Represents a single seat in the workspace.
+
+    Attributes: 
+        free: (bool): True if the seat is available.
+        occupant (str): Name of the person occupying the seat.
+    """
+    def __init__(self, free: bool = True, occupant: str = "") -> None:
+        
+        self.free: bool = free
+        self.occupant: str = occupant
+    
+    def set_occupant(self,name: str) -> None:
+        """Assign a person to the seat if it's free."""
+       
+        if self.free:
+            self.occupant = name
+            self.free = False
+            print(f"{name} has been assigned to the seat.")
+        else:
+            print(f"Seat is already occupied by {self.occupant}.")
+
+    def remove_occupant(self) -> str:
+        """Remove the person from the seat and make it free again."""
+       
+        if self.free == False:
+            name = self.occupant
+            self.occupant = " "
+            self.free = True
+            print(f"{name} has left the seat")
+            return name
+        else:
+            print("Seat is already free")
+            return None
+    
+    def __str__(self) -> str:
+        """Return a readable representation of the seat."""
+        
+        if self.free:
+            return "Empty seat"
+        else: 
+            return f"Seat occupied by {self.occupant})"
+
+class Table:
+    """ 
+    Represents a table containing several seats.
+
+    Attributes: 
+        capacity(int): number of seats at the table
+        seats (list): a list of seat objects
+    """
+    def __init__(self, capacity: int = 4) -> None:
+       
+        self.capacity = capacity
+        # Create an empty list to hold the seats
+        seats = []
+        # Add Seat() objects one by one
+        for _ in range(capacity):
+            new_seat = Seat()       # create a new Seat object
+            seats.append(new_seat)  # add it to the list
+        self.seats = seats          # assign the finished list to the attribute
+    
+    def has_free_spot(self) -> bool:
+        """Return True if at least one seat is free."""
+        
+        for seat in self.seats:
+            if seat.free:
+                print("A seat is available.")
+                return True
+        print(f"No free seats available.")
+        return False
+    
+    def assign_seat(self,name: str) -> bool:
+        """Assign a person to the first free seat."""
+        
+        for seat in self.seats:
+            if seat.free:
+                seat.set_occupant(name)
+                return True
+        
+        print(f"No free seats available for {name}.")
+        return False
+    
+    def left_capacity(self) -> int:
+        """Return the number of free seats left."""
+        
+        free_count = 0               # Start counting from zero
+        for seat in self.seats:      # Loop through all seats
+            if seat.free:            # Check if this seat is free
+                free_count += 1      # Increase our counter
+        return free_count            # Return the final count
+    
+    def __str__(self) -> str:
+        """Return a readable summary of the table"""
+        
+        return f"This table has a capacity of {self.capacity} and has {self.left_capacity} seats left."
+    
+
+    
 
